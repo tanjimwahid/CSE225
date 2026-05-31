2c18c276e44ee129a1c83867364202027a6b2061
tanjimwahid <90888076+tanjimwahid@users.noreply.github.com>
2026-05-31 11:58:47 +0600

    Doing basic math in c++

---
+++ C++_Fundamental/var_and_DType/basicMath.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a = 10;
+    int b = 20;
+
+    std::cout << "Value of a + b = " << a + b << std::endl;
+
+    return 0;
+}
---
+++ C++_Fundamental/var_and_DType/code.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    std::cout << "****\n***\n**\n*\n";
+    return 0;
+}
