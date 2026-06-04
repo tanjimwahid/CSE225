47c8a70a49277221f95d6a55f69fae0f191a2427
tanjimwahid <90888076+tanjimwahid@users.noreply.github.com>
2026-06-04 22:25:22 +0600

    Type casting in c++

--- C++_Fundamental/Operator/typeConvo.cpp
+++ C++_Fundamental/Operator/typeConvo.cpp
@@ -1,18 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
     // Implicit Type Conversion
     int num1 = 10;
-    double num2 = num1;  // int -> double
+    double num2 = num1; // int -> double
 
     cout << "Implicit Conversion:" << endl;
-    cout << "Integer value: " << num1 << endl;
-    cout << "Converted to double: " << num2 << endl;
+    cout << "Integer value: 10/3 = " << 10 / 3 << endl;
+    cout << "Converted to double: 10/3.0 = " << 10 / 3.0 << endl;
 
     // Explicit Type Conversion (Type Casting)
     double price = 99.99;
-    int roundedPrice = (int)price;  // double -> int
+    int roundedPrice = (int)price; // double -> int
 
     cout << "\nExplicit Conversion:" << endl;
     cout << "Double value: " << price << endl;
