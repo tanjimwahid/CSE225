fb30c1da0aef19d844b16599e5dd947ca8f136a9
tanjimwahid <90888076+tanjimwahid@users.noreply.github.com>
2026-06-16 23:18:51 +0600

    Class 01 of lab 225

---
+++ LAB_Exercises/class01/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    cout << "Enter Your Name Sir: ";
+    cin >> name;
+    cout << "Hello: " << name << endl;
+    return 0;
+}
---
+++ LAB_Exercises/class01/sumOf2Numbers.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int a, b;
+    cout << "Input 2 Numbers ";
+    cin >> a >> b;
+
+    cout << "Sum " << a + b;
+
+    return 0;
+}
