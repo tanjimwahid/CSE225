d184e720e8470f2e441d2fb108cd6e82a73beb02
tanjimwahid <90888076+tanjimwahid@users.noreply.github.com>
2026-07-31 19:33:08 +0600

    dynamic allocation

--- Pointer/delete ptr/main.cpp
+++ Pointer/delete ptr/main.cpp
@@ -4,5 +4,15 @@ using namespace std;
 int main()
 {
 
-    cout << "Hello World";
+    cout << "Hello World" << endl;
+
+    int x = 8, y = 10;
+    int *ptr1 = new int;
+    *ptr1 = 8;
+    int *ptr2 = new int;
+    *ptr2 = 10;
+    ptr2 = ptr1;
+    delete ptr1;
+    cout << ptr2 << endl;
+    cout << ptr1 << endl;
 }
